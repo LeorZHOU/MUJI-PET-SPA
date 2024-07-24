635642fae717b64c4e55952dfff399863e744a45
Leor <Leor99927@gmail.com>
2024-07-24 17:14:02 +0800

    新增服務項目 品牌介紹 安親須知按鈕等功能

--- index.js
+++ index.js
@@ -14,18 +14,29 @@
 let petide = document.querySelector("#petide");
 
 function changeImage() {
-  let myImgSrc = petide.getAttribute("src"); // Get the current src attribute value
+  let myImgSrc = petide.getAttribute("src");
 
-  if (myImgSrc === "./assets/petideimg.jpg") {
-    petide.setAttribute("src", "./assets/petideimg2.jpg"); // Set the new src attribute
-  } else if (myImgSrc === "./assets/petideimg2.jpg") {
-    petide.setAttribute("src", "./assets/petideimg3.jpg");
-  } else if (myImgSrc === "./assets/petideimg3.jpg") {
-    petide.setAttribute("src", "./assets/petideimg4.jpg");
+  if (myImgSrc === "./assets/MUJI04.jpg") {
+    petide.setAttribute("src", "./assets/MUJI02.jpg");
+  } else if (myImgSrc === "./assets/MUJI02.jpg") {
+    petide.setAttribute("src", "./assets/MUJI01.jpg");
+  } else if (myImgSrc === "./assets/MUJI01.jpg") {
+    petide.setAttribute("src", "./assets/MUJI03.jpg");
   } else {
-    petide.setAttribute("src", "./assets/petideimg.jpg");
+    petide.setAttribute("src", "./assets/MUJI04.jpg");
   }
 }
-
-// Set the interval to change the image every 2 seconds (2000 milliseconds)
 setInterval(changeImage, 2000);
+
+document.addEventListener("DOMContentLoaded", () => {
+  let dialog = document.querySelector(".dialog");
+  let btn = document.querySelector(".btn");
+  btn.addEventListener("click", () => {
+    dialog.style.display = "block";
+  });
+  let closeDialog = document.querySelector(".closeDialog");
+  closeDialog.addEventListener("click", () => {
+    dialog.style.display = "none";
+    // console.log("Hello");
+  });
+});
---
+++ serviceitem.js
@@ -0,0 +1,26 @@
+let petide = document.querySelector("#petide");
+
+function changeImage() {
+  let myImgSrc = petide.getAttribute("src");
+
+  if (myImgSrc === "./assets/serviceimg/001.png") {
+    petide.setAttribute("src", "./assets/serviceimg/002.png");
+  } else if (myImgSrc === "./assets/serviceimg/002.png") {
+    petide.setAttribute("src", "./assets/serviceimg/003.png");
+  } else if (myImgSrc === "./assets/serviceimg/003.png") {
+    petide.setAttribute("src", "./assets/serviceimg/005.png");
+  } else if (myImgSrc === "./assets/serviceimg/005.png") {
+    petide.setAttribute("src", "./assets/serviceimg/006.png");
+  } else if (myImgSrc === "./assets/serviceimg/006.png") {
+    petide.setAttribute("src", "./assets/serviceimg/007.png");
+  } else if (myImgSrc === "./assets/serviceimg/007.png") {
+    petide.setAttribute("src", "./assets/serviceimg/008.png");
+  } else if (myImgSrc === "./assets/serviceimg/008.png") {
+    petide.setAttribute("src", "./assets/serviceimg/009.png");
+  } else if (myImgSrc === "./assets/serviceimg/009.png") {
+    petide.setAttribute("src", "./assets/serviceimg/010.png");
+  } else {
+    petide.setAttribute("src", "./assets/serviceimg/001.png");
+  }
+}
+setInterval(changeImage, 2000);
